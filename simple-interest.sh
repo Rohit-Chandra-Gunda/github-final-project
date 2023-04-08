55947497b1dc74ea10534da94f07d693ab7b6109
Rohit Chandra Gunda <grohitchandra@gmail.com>
2023-04-08 14:24:01 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <Rohit-Chandra-Gunda>
+   # Rohit-Chandra-Gunda
    # Input:
    # p, principal amount
    # t, time period in years
